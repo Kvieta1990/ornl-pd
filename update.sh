4095441d10952425a5eeeaa70c812c4b6674db41
Yuanpeng Zhang <zyroc1990@gmail.com>
2023-04-16 14:26:46 -0400

    update

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+eval "$(conda shell.bash hook)"
+conda activate jbook
+jupyter-book build doc && a=1 || a=0
+if [ $a = 1 ] ; then
+    git add . --all
+    git commit -m "update"
+    git push
+    ssh iris /mnt/Iris_ExtL/update_pd.sh
+fi
+conda deactivate
