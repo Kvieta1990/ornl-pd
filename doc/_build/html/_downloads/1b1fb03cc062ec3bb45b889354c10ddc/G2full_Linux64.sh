e97672d37959f7e0cfc56a7921c8904e3961c2ff
Yuanpeng Zhang <zyroc1990@gmail.com>
2026-05-19 12:10:29 -0400

    comment out announcement

--- doc/_build/html/_downloads/1b1fb03cc062ec3bb45b889354c10ddc/G2full_Linux64.sh
+++ doc/_build/html/_downloads/1b1fb03cc062ec3bb45b889354c10ddc/G2full_Linux64.sh
@@ -10,7 +10,7 @@ gitCodeRepo=https://github.com/Kvieta1990/GSAS-II.git
 pyver=3.11
 numpyver=1.26
 
-packages="python=$pyver wxpython numpy=$numpyver scipy matplotlib pyopengl conda anaconda-client git gitpython requests pillow h5py imageio scons cython seekpath"
+packages="python=$pyver wxpython numpy=$numpyver scipy matplotlib pyopengl conda anaconda-client git gitpython requests pillow h5py imageio scons cython seekpath sympy pybaselines pycifrw xmltodict zarr"
 
 env=testG2
 miniforge=https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
@@ -19,26 +19,26 @@ install=True
 gitinstall=True
 
 if [ "$install" = "True" ]; then
-	rm -rf $condaHome
-	if [ ! -e "/tmp/Miniforge-latest.sh" ]; then
-		echo Downloading
-		curl -L $miniforge -o /tmp/Miniforge-latest.sh
-	else
-		echo "skipping miniconda download"
-	fi
-	if [ ! -d "$condaHome" ]; then
-		echo creating conda installation
-		bash /tmp/Miniforge-latest.sh -b -p $condaHome
-	else
-		echo "skip miniconda install"
-	fi
+    rm -rf $condaHome
+    if [ ! -e "/tmp/Miniforge-latest.sh" ]; then
+        echo Downloading
+        curl -L $miniforge -o /tmp/Miniforge-latest.sh
+    else
+        echo "skipping miniconda download"
+    fi
+    if [ ! -d "$condaHome" ]; then
+        echo creating conda installation
+        bash /tmp/Miniforge-latest.sh -b -p $condaHome
+    else
+        echo "skip miniconda install"
+    fi
 fi
 
 echo source $condaHome/bin/activate
 source $condaHome/bin/activate
 
 if [ "$install" = "True" ]; then
-	conda create -y -n $env $packages
+    conda create -y -n $env $packages
 fi
 
 set +x
@@ -46,23 +46,23 @@ echo source $condaHome/bin/activate $env
 source $condaHome/bin/activate $env
 
 if [ "$gitinstall" = "True" ]; then
-	set -x
-	rm -rf $WORKSPACE/GSAS2-code
-	mkdir $WORKSPACE/GSAS2-code
-	git clone -b main $gitCodeRepo $WORKSPACE/GSAS2-code --depth 50
+    set -x
+    rm -rf $WORKSPACE/GSAS2-code
+    mkdir $WORKSPACE/GSAS2-code
+    git clone -b main $gitCodeRepo $WORKSPACE/GSAS2-code --depth 50
 fi
 
 directory="$WORKSPACE/GSAS2-code/GSASII/bindist"
 
 if [ ! -d "$directory" ]; then
-	mkdir "$directory"
-	echo "Directory $directory created."
+    mkdir "$directory"
+    echo "Directory $directory created."
 else
-	echo "Directory $directory already exists."
+    echo "Directory $directory already exists."
 fi
 
 download_binaries() {
-	$condaHome/bin/python - <<END
+    $condaHome/bin/python - <<END
 def download_binaries():
     import urllib.request
     import tarfile
